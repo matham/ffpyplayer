6edc8febea4388083a7e7fa333dc4091e5ab4941
Matthew Einhorn <moiein2000@gmail.com>
2014-05-28 15:10:49 -0400

    Add debuging info to test.py.

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+'''
+To run, please provide a filename on the command line when running the file.
+'''
+
+
 import kivy
 from kivy.base import EventLoop
 EventLoop.ensure_window()
@@ -11,6 +16,8 @@ from kivy.lang import Builder
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.weakmethod import WeakMethod
 import sys
+import logging
+logging.root.setLevel(logging.DEBUG)
 
 
 Builder.load_string('''
@@ -45,11 +52,17 @@ Builder.load_string('''
 class Root(RelativeLayout):
     pass
 
-loglevel_emit = 'error'
+log_level = 'debug'
+logger_func = {'quiet': logging.critical, 'panic': logging.critical,
+               'fatal': logging.critical, 'error': logging.error,
+               'warning': logging.warning, 'info': logging.info,
+               'verbose': logging.debug, 'debug': logging.debug}
+
+
 def log_callback(message, level):
     message = message.strip()
-    if message and loglevels[level] <= loglevels[loglevel_emit]:
-        print '%s: %s' %(level, message.strip())
+    if message:
+        logger_func[level]('ffpyplayer: {}'.format(message))
 
 
 class PlayerApp(App):
@@ -66,13 +79,13 @@ class PlayerApp(App):
         return self.root
 
     def on_start(self):
-        global loglevel_emit
-        loglevel_emit = 'info'
         self.callback_ref = WeakMethod(self.callback)
         filename = sys.argv[1]
-        ff_opts = {}# try ff_opts = {'vf':'edgedetect'} http://ffmpeg.org/ffmpeg-filters.html
+        logging.info('ffpyplayer: Playing file "{}"'.format(filename))
+        # try ff_opts = {'vf':'edgedetect'} http://ffmpeg.org/ffmpeg-filters.html
+        ff_opts = {}
         self.ffplayer = MediaPlayer(filename, callback=self.callback_ref,
-                                    loglevel='info', ff_opts=ff_opts)
+                                    loglevel=log_level, ff_opts=ff_opts)
         Clock.schedule_once(self.redraw, 0)
         self.keyboard = Window.request_keyboard(None, self.root)
         self.keyboard.bind(on_key_down=self.on_keyboard_down)
@@ -86,6 +99,7 @@ class PlayerApp(App):
                 self.ffplayer.set_size(-1, self.root.image.height)
             else:
                 self.ffplayer.set_size(self.root.image.width, -1)
+            logging.debug('ffpyplayer: Resized video.')
 
     def update_pts(self, *args):
         if self.ffplayer:
@@ -96,13 +110,17 @@ class PlayerApp(App):
             return False
         ctrl = 'ctrl' in modifiers
         if keycode[1] == 'p' or keycode[1] == 'spacebar':
+            logging.info('Toggled pause.')
             self.ffplayer.toggle_pause()
             Clock.unschedule(self.redraw)
             Clock.schedule_once(self.redraw, 0)
         elif keycode[1] == 'r':
+            logging.debug('ffpyplayer: Forcing a refresh.')
             self.redraw(force_refresh=True)
         elif keycode[1] == 'v':
-            self.ffplayer.request_channel('video', 'close' if ctrl else 'cycle')
+            logging.debug('ffpyplayer: Changing video stream.')
+            self.ffplayer.request_channel('video',
+                                          'close' if ctrl else 'cycle')
             Clock.unschedule(self.update_pts)
             Clock.unschedule(self.redraw)
             if ctrl:    # need to continue updating pts, since video is disabled.
@@ -110,32 +128,41 @@ class PlayerApp(App):
             else:
                 Clock.schedule_once(self.redraw, 0)
         elif keycode[1] == 'a':
-            self.ffplayer.request_channel('audio', 'close' if ctrl else 'cycle')
+            logging.debug('ffpyplayer: Changing audio stream.')
+            self.ffplayer.request_channel('audio',
+                                          'close' if ctrl else 'cycle')
         elif keycode[1] == 't':
-            self.ffplayer.request_channel('subtitle', 'close' if ctrl else 'cycle')
+            logging.debug('ffpyplayer: Changing subtitle stream.')
+            self.ffplayer.request_channel('subtitle',
+                                          'close' if ctrl else 'cycle')
         elif keycode[1] == 'right':
+            logging.debug('ffpyplayer: Seeking forward by 10s.')
             self.ffplayer.seek(10.)
             self.next_frame = None
             Clock.unschedule(self.redraw)
             Clock.schedule_once(self.redraw, 0)
         elif keycode[1] == 'left':
+            logging.debug('ffpyplayer: Seeking back by 10s.')
             self.ffplayer.seek(-10.)
             self.next_frame = None
             Clock.unschedule(self.redraw)
             Clock.schedule_once(self.redraw, 0)
         elif keycode[1] == 'up':
+            logging.debug('ffpyplayer: Increasing volume.')
             self.ffplayer.set_volume(self.ffplayer.get_volume() + 0.01)
             self.root.volume.value = self.ffplayer.get_volume()
         elif keycode[1] == 'down':
+            logging.debug('ffpyplayer: Decreasing volume.')
             self.ffplayer.set_volume(self.ffplayer.get_volume() - 0.01)
             self.root.volume.value = self.ffplayer.get_volume()
         return True
 
     def touch_down(self, touch):
         if self.root.seek.collide_point(*touch.pos) and self.ffplayer:
-            self.ffplayer.seek((touch.pos[0] - self.root.volume.width) / \
-                               self.root.seek.width * self.ffplayer.get_metadata()['duration'],
-                               relative=False)
+            pts = ((touch.pos[0] - self.root.volume.width) /
+            self.root.seek.width * self.ffplayer.get_metadata()['duration'])
+            logging.debug('ffpyplayer: Seeking to {}.'.format(pts))
+            self.ffplayer.seek(pts, relative=False)
             self.next_frame = None
             Clock.unschedule(self.redraw)
             Clock.schedule_once(self.redraw, 0)
@@ -146,11 +173,13 @@ class PlayerApp(App):
         if self.ffplayer is None:
             return
         if selector == 'quit':
+            logging.debug('ffpyplayer: Quitting.')
             def close(*args):
                 Clock.unschedule(self.redraw)
                 self.ffplayer = None
             Clock.schedule_once(close, 0)
-        elif selector == 'display_sub': # called from internal thread, it typically reads forward
+        # called from internal thread, it typically reads forward
+        elif selector == 'display_sub':
             self.display_subtitle(*value)
 
     def redraw(self, dt=0, force_refresh=False):
@@ -161,32 +190,45 @@ class PlayerApp(App):
             self.next_frame = None
             if img.get_size() != self.size or self.texture is None:
                 self.root.image.canvas.remove_group(str(self)+'_display')
-                self.texture = Texture.create(size=img.get_size(), colorfmt='rgb')
-                # by adding 'vf':'vflip' to the player initialization ffmpeg will do the flipping
+                self.texture = Texture.create(size=img.get_size(),
+                                              colorfmt='rgb')
+                # by adding 'vf':'vflip' to the player initialization ffmpeg
+                # will do the flipping
                 self.texture.flip_vertical()
                 self.texture.add_reload_observer(self.reload_buffer)
                 self.size = img.get_size()
+                logging.debug('ffpyplayer: Creating new image texture of '
+                              'size: {}.'.format(self.size))
             self.buffer = bytes(img.to_bytearray()[0])
             self.texture.blit_buffer(self.buffer)
             self.root.image.texture = None
             self.root.image.texture = self.texture
             self.root.seek.value = pts
-        self.next_frame, val = self.ffplayer.get_frame(force_refresh=force_refresh)
+            logging.debug('ffpyplayer: Blitted new frame with time: {}.'
+                          .format(pts))
+        self.next_frame, val = self.ffplayer.get_frame(force_refresh=
+                                                       force_refresh)
         if val == 'eof':
-            print 'eof'
+            logging.debug('ffpyplayer: Got eof.')
             return
         elif val == 'paused':
-            print 'paused'
+            logging.debug('ffpyplayer: Got paused.')
             return
-        Clock.schedule_once(self.redraw, val if val or self.next_frame else 1/60.)
+        else:
+            logging.debug('ffpyplayer: Next frame scheduled at {}.'
+                          .format(val))
+        Clock.schedule_once(self.redraw,
+                            val if val or self.next_frame else 1/60.)
         if self.root.seek.value:
-            self.root.seek.max = self.ffplayer.get_metadata()['duration'] # do once
+            self.root.seek.max = self.ffplayer.get_metadata()['duration']
 
     def display_subtitle(self, text, fmt, pts, t_start, t_end):
         pass # fmt is text (unformatted), or ass (formatted subs)
 
     def reload_buffer(self, *args):
-        self.texture.blit_buffer(self.buffer, colorfmt='rgb', bufferfmt='ubyte')
+        logging.debug('ffpyplayer: Reloading buffer.')
+        self.texture.blit_buffer(self.buffer, colorfmt='rgb',
+                                 bufferfmt='ubyte')
 
 if __name__ == '__main__':
     set_log_callback(log_callback)
