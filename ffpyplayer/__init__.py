a213b8c96b74812244b9cbb0d5a54ee01708fb4c
Matthew Einhorn <moiein2000@gmail.com>
2016-01-05 04:10:52 -0500

    Add .travis.yaml

--- ffpyplayer/__init__.py
+++ ffpyplayer/__init__.py
@@ -1,7 +1,7 @@
 
 import sys
+import os
 from os.path import join, isdir
-import platform
 
 __version__ = '4.0.dev0'
 
@@ -10,26 +10,10 @@ _ffmpeg_git = 'c413d9e6356e843aa492be9bb0ddf66ae6c97501'
 # skipped all show modes and subtitle display related functionality commits
 
 
-try:
-    if platform.system() != 'Windows':
-        raise ImportError()
+_ffmpeg = join(sys.prefix, 'share', 'ffpyplayer', 'ffmpeg', 'bin')
+if isdir(_ffmpeg):
+    os.environ["PATH"] += os.pathsep + _ffmpeg
 
-    import ctypes
-    try:
-        _AddDllDirectory = ctypes.windll.kernel32.AddDllDirectory
-        _AddDllDirectory.argtypes = [ctypes.c_wchar_p]
-        # Needed to initialize AddDllDirectory modifications
-        ctypes.windll.kernel32.SetDefaultDllDirectories(0x1000)
-    except AttributeError:
-        _AddDllDirectory = ctypes.windll.kernel32.SetDllDirectoryW
-        _AddDllDirectory.argtypes = [ctypes.c_wchar_p]
-
-    _ffmpeg = join(sys.prefix, 'share', 'ffpyplayer', 'ffmpeg', 'bin')
-    if isdir(_ffmpeg):
-        _AddDllDirectory(_ffmpeg)
-
-    _sdl = join(sys.prefix, 'share', 'ffpyplayer', 'sdl', 'bin')
-    if isdir(_sdl):
-        _AddDllDirectory(_sdl)
-except ImportError:
-    pass
+_sdl = join(sys.prefix, 'share', 'ffpyplayer', 'sdl', 'bin')
+if isdir(_sdl):
+    os.environ["PATH"] += os.pathsep + _sdl
