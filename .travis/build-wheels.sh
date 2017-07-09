4192f71af6b2a5c6343caa410c6634a6a30f09c6
Matthew Einhorn <moiein2000@gmail.com>
2017-07-09 15:23:03 -0400

    Quiet ssl error.

--- .travis/build-wheels.sh
+++ .travis/build-wheels.sh
@@ -58,7 +58,7 @@ make install;
 make distclean;
 
 cd ~/ffmpeg_sources;
-wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz;
+wget --no-check-certificate http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz;
 tar xzf lame-3.99.5.tar.gz;
 cd lame-3.99.5;
 ./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --enable-shared;
