cd944c46ea67eab511cfb327d706c5e580077d87
matham <moiein2000@gmail.com>
2018-05-27 11:41:30 -0400

    Conda doesn't have x264 encoder.

--- ffpyplayer/tests/test_write.py
+++ ffpyplayer/tests/test_write.py
@@ -13,13 +13,19 @@ class PicTestCase(unittest.TestCase):
         from ffpyplayer.writer import MediaWriter
         from ffpyplayer.tools import get_supported_pixfmts, get_supported_framerates
         from ffpyplayer.pic import Image
+        from ffpyplayer.tools import get_codecs
+
+        lib_opts = {}
+        codec = 'rawvideo'
+        if 'libx264' in get_codecs(encode=True, video=True):
+            codec = 'libx264'
+            lib_opts = {'preset':'slow', 'crf':'22'}
 
         w, h = 640, 480
         out_opts = {
             'pix_fmt_in': 'rgb24', 'width_in': w, 'height_in': h,
-            'codec': 'libx264', 'frame_rate': (5, 1)}
+            'codec': codec, 'frame_rate': (5, 1)}
 
-        lib_opts = {'preset':'slow', 'crf':'22'}
         metadata = {'title':'Singing in the sun', 'author':'Rat', 'genre':'Animal sounds'}
         writer = MediaWriter(fname, [out_opts] * 2, fmt='mp4',
                              width_out=w/2, height_out=h/2, pix_fmt_out='yuv420p',
