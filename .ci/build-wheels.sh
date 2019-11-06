0fc016c982b04458d700cce6d50d50c6940ce082
Matthew Einhorn <moiein2000@gmail.com>
2019-11-05 21:36:39 -0500

    Add support for more sdl2 components in manylinux.

--- .ci/build-wheels.sh
+++ .ci/build-wheels.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e -x
 
-yum -y install libass libass-devel autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel enca-devel fontconfig-devel openssl openssl-devel wget
+yum -y install libass libass-devel autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial \
+pkgconfig zlib-devel enca-devel fontconfig-devel openssl openssl-devel wget openjpeg openjpeg-devel \
+libpng libpng-devel libtiff libtiff-devel libwebp libwebp-devel dbus-devel dbus ibus-devel ibus libsamplerate-devel \
+libsamplerate libudev-devel libudev libmodplug-devel libmodplug flac-devel flac \
+libjpeg-turbo-devel libjpeg-turbo
 mkdir ~/ffmpeg_sources;
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/ffmpeg_build/lib;
 
