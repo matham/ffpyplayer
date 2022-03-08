76c376345600bdadea3e55b09bb0e7a8f4fa8063
Matthew Einhorn <matt@einhorn.dev>
2022-03-07 21:27:18 -0500

    CXX flags must also be cleared.

--- .ci/build_wheels_osx.sh
+++ .ci/build_wheels_osx.sh
@@ -42,7 +42,7 @@ cd "$SRC_PATH";
 curl -sLO "https://github.com/libsdl-org/SDL/releases/download/release-$SDL_VERSION/SDL2-$SDL_VERSION.tar.gz"
 tar xzf "SDL2-$SDL_VERSION.tar.gz"
 cd "SDL2-$SDL_VERSION"
-./configure --prefix="$BUILD_PATH" --bindir="$BUILD_PATH/bin" CC="sh build-scripts/clang-fat.sh" CFLAGS=""
+./configure --prefix="$BUILD_PATH" --bindir="$BUILD_PATH/bin" CC="sh build-scripts/clang-fat.sh" CFLAGS="" CXXFLAGS=""
 make
 make install
 make distclean
