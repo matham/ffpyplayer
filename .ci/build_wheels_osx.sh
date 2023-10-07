5ad25509e875b75cc3f1dbca8275a335c8e52278
Mirko Galimberti <me@mirkogalimberti.com>
2023-10-07 17:37:31 +0200

    Update zlib download url for macOS dependencies

--- .ci/build_wheels_osx.sh
+++ .ci/build_wheels_osx.sh
@@ -40,7 +40,7 @@ make install
 
 
 cd "$SRC_PATH"
-curl -sLO "https://zlib.net/zlib-$ZLIB_VERSION.tar.gz"
+curl -sLO "https://zlib.net/fossils/zlib-$ZLIB_VERSION.tar.gz"
 tar xzf "zlib-$ZLIB_VERSION.tar.gz"
 cd "zlib-$ZLIB_VERSION"
 ./configure --prefix="$BUILD_PATH"
