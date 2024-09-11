f322ab946449a377ef006259952de530a3174422
Mirko Galimberti <me@mirkogalimberti.com>
2024-09-11 21:44:56 +0200

    Add support for Python 3.13

--- .ci/merge_osx_deps.sh
+++ .ci/merge_osx_deps.sh
@@ -6,7 +6,7 @@ py_osx_ver=$(echo ${MACOSX_DEPLOYMENT_TARGET} | sed "s/\./_/g")
 py_osx_ver_arm=$(echo ${MACOSX_DEPLOYMENT_TARGET_ARM} | sed "s/\./_/g")
 for whl in *.whl; do
    if [[ "$whl" == *macosx_${py_osx_ver}_x86_64.whl ]]; then
-       whl_base=$(echo "$whl" | rev | cut -c 23- | rev)
+       whl_base=$(echo "$whl" | rev | cut -c 24- | rev)
        if [[ -f "${whl_base}macosx_${py_osx_ver_arm}_arm64.whl" ]]; then
            delocate-merge "$whl" "${whl_base}macosx_${py_osx_ver_arm}_arm64.whl"
        fi
